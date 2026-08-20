65c42c0156dfcfa52301ebd59b9dd1974020007e
Ivan <ivanbeatz8305@example.com>
2026-08-20 09:02:17 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -172,7 +172,7 @@ class ProfilePage extends StatelessWidget {
                   ),
                   TextButton.icon(
                     onPressed: () {
-                      showMessage(context, 'Thanks for viewing my profile!');
+                      showMessage(context, 'Thanks for viewing my profil!');
                     },
                     icon: Icon(Icons.thumb_up),
                     label: Text('Like Profile'),
